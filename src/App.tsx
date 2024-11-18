c59730cf367d132615a0188d4bd9931570ec01ca
Alex Kozhenov <kozhenov1984@mail.ru>
2024-11-18 21:39:48 +0400

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import Header from "./components/header/Header";
 
 function App() {
-
   return (
     <>
-      <h1>Christmas Shop</h1>
+      <Header></Header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
