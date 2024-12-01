522b79e932591d57c593a597a7fc998d8c47e207
Alex Kozhenov <kozhenov1984@mail.ru>
2024-12-01 21:22:46 +0400

    change Header in accordance with new requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Home />
-    </>
+    </BrowserRouter>
   );
 }
 
